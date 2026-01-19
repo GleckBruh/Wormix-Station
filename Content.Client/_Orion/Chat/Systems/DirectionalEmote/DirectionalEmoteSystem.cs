008ebd0bea643e91f075646649280c5709e3eeb8
PuroSlavKing <103608145+puroslavking@users.noreply.github.com>
2026-01-19 18:46:32 +0200

    [Add] Directional emote and fixes from old Wormix

---
+++ Content.Client/_Orion/Chat/Systems/DirectionalEmote/DirectionalEmoteSystem.cs
@@ -0,0 +1,66 @@
+using Content.Shared._Orion.Chat.Components;
+using Content.Shared.CCVar;
+using Content.Shared.Ghost;
+using Content.Shared.Inventory;
+using Content.Shared.Verbs;
+using Robust.Client.UserInterface;
+using Robust.Shared.Configuration;
+using Robust.Shared.Utility;
+
+namespace Content.Client._Orion.Chat.Systems.DirectionalEmote;
+
+public sealed partial class DirectionalEmoteSystem : EntitySystem
+{
+    [Dependency] private readonly IUserInterfaceManager _userInterfaceManager = default!;
+    [Dependency] private readonly IConfigurationManager _cfg = default!;
+
+    private bool _directionalEmotesEnabled;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        _cfg.OnValueChanged(CCVars.DirectionalEmotesEnabled, v => _directionalEmotesEnabled = v, true);
+
+        SubscribeLocalEvent<DirectionalEmoteTargetComponent, GetVerbsEvent<Verb>>(OnGetVerbs);
+    }
+
+    public void ShowMessage(NetEntity source, NetEntity target, string text)
+    {
+        if (string.IsNullOrEmpty(text))
+            return;
+
+        var maxLength = _cfg.GetCVar(CCVars.ChatMaxEmoteLength);
+        if (text.Length > maxLength)
+            return;
+
+        RaiseNetworkEvent(new SendDirectionalEmoteEvent(source, target, text));
+    }
+
+    private void OnGetVerbs(EntityUid uid, DirectionalEmoteTargetComponent component, GetVerbsEvent<Verb> args)
+    {
+        if (!_directionalEmotesEnabled)
+            return;
+
+        if (args.Target == args.User ||
+            !HasComp<DirectionalEmoteTargetComponent>(args.User))
+            return;
+
+        if (HasComp<GhostComponent>(args.User) && !HasComp<InventoryComponent>(args.User)) // Wormix edit
+            return; // Who we lost can't speak to us.. But admin can
+
+        args.Verbs.Add(new Verb
+        {
+            Act = () => OpenWindow(GetNetEntity(args.User), GetNetEntity(args.Target)),
+            Priority = 15,
+            Text = Loc.GetString("directional-emote-verb-name"),
+            Icon = new SpriteSpecifier.Texture(new ResPath("/Textures/Interface/emotes.svg.192dpi.png")),
+            ClientExclusive = true,
+        });
+    }
+
+    private void OpenWindow(NetEntity source, NetEntity target)
+    {
+        _userInterfaceManager.GetUIController<DirectionalEmoteUIController>().ToggleWindow(source, target);
+    }
+}
